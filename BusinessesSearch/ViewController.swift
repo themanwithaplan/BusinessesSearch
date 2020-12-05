edca82e477885574c1a0aed906785d50805d2107
TheManWithAPlan <nafeesnazaar@gmail.com>
2020-12-04 20:15:01 -0800

    Initial Commit

---
+++ BusinessesSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BusinessesSearch
+//
+//  Created by test on 12/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
